8d1aa0c70d8d9708e3fc51174aa1eef2bb12647a
salimxfardeheb <salimfardeheb442@gmail.com>
2024-10-01 20:01:53 +0100

    initialize project

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./index.css";
+
+// components
+import Navbar from "./components/navbar";
+
+function App() {
+  return <div className="">
+    <Navbar />
+  </div>;
+}
+
+export default App;
---
+++ client/src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+      <div className='bg-sky-800 p-4 flex justify-center'>
+        <h1>hello</h1>
+
+      </div>
+      )
+}
+
+export default Navbar
