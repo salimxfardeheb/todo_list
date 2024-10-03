21445aac8ae048a54d87601bd3e86815e16edad9
salimxfardeheb <salimfardeheb442@gmail.com>
2024-10-03 16:41:38 +0100

    feat sort tasks and feat tabs done, todos'

--- client/src/components/tabs.js
+++ client/src/components/tabs.js
@@ -1,43 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdDelete } from "react-icons/md";
 
-const tabs = () => {
+const tasksExpl = [
+  {
+    id: 1,
+    title: "learn node js",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "learn react js",
+    completed: true,
+  },
+  {
+    id: 3,
+    title: "learn git",
+    completed: false,
+  },
+];
+
+const Tabs = () => {
+  const [tasks, setTasks] = useState(tasksExpl);
+  const [activeTab, setActiveTab] = useState("all");
+
+  const sortedtasks = [...tasks].sort((a, b) => a.completed - b.completed);
+
+  const toggleCompletion = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id == id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+  const filteredTasks = () => {
+    if (activeTab === "done") return sortedtasks.filter((task) => task.completed);
+    if (activeTab === "todos") return sortedtasks.filter((task) => !task.completed);
+    return sortedtasks;
+  };
   return (
     <div className="mx-7 flex flex-col justify-center mt-6 gap-3">
       <h2 className="text-white text-2xl font-semibold">Tasks</h2>
       <div className="flex text-white justify-center gap-2">
-        <button className="tabsbtn rounded-l-md">All</button>
-        <button className="tabsbtn">Done</button>
-        <button className="tabsbtn rounded-r-md">Todos</button>
+        <button
+          className={`tabsbtn rounded-l-md ${
+            activeTab === "all" ? "active" : ""
+          }`}
+          onClick={() => setActiveTab("all")}
+        >
+          All
+        </button>
+        <button
+          className={`tabsbtn ${activeTab === "done" ? "active" : ""}`}
+          onClick={() => setActiveTab("done")}
+        >
+          Done
+        </button>
+        <button
+          className={`tabsbtn rounded-r-md ${
+            activeTab === "todos" ? "active" : ""
+          }`}
+          onClick={() => setActiveTab("todos")}
+        >
+          Todos
+        </button>
       </div>
-      <div id="all">{/*all tasks */}</div>
-      <div id="done">
-        {/*tasks done*/}
-        <div className="flex gap-6 items-center justify-around mx-6">
-        <div class="checkbox-wrapper-15 inline-block align-middle translate ">
-            <input
-              class="inp-cbx hidden"
-              id="cbx-15"
-              type="checkbox"
-
-            />
-
-
-            <label class="cbx" for="cbx-15">
-              <span>
-                <svg width="30px" height="26px" viewbox="0 0 12 9">
-                <polyline points="1 5 4 8 11 1"></polyline>
-                </svg>
-              </span>
-            </label>
+      <div id={activeTab}>
+        {filteredTasks().map((task) => (
+          <div
+            className="flex gap-6 items-center justify-around mx-6 py-4"
+            key={task.id}
+          >
+            <div className="checkbox-wrapper-15">
+              <input
+                className="inp-cbx hidden"
+                id={task.id}
+                type="checkbox"
+                checked={task.completed}
+                onClick={() => {
+                  toggleCompletion(task.id);
+                }}
+              />
+              <label className="cbx" for={task.id}>
+                <span>
+                  <svg width="30px" height="26px" viewbox="0 0 12 9">
+                    <polyline points="1 5 4 8 11 1"></polyline>
+                  </svg>
+                </span>
+              </label>
+            </div>
+            <div className="bg-white rounded-md w-full px-3 py-1">
+              <p className="text-xl font-semibold">{task.title}</p>
+            </div>
+            <div className="bg-red-700 p-2 rounded-full">
+              <MdDelete className="text-white text-2xl" />
+            </div>
           </div>
-          <div className="bg-white rounded-md w-full px-3 py-1"><p className="text-xl font-semibold">task title</p></div>
-          <div className="bg-red-700 p-2 rounded-full"><MdDelete className="text-white text-2xl"/></div>
-        </div>
+        ))}
       </div>
-      <div id="todos">{/*tasks todo */}</div>
     </div>
   );
 };
 
-export default tabs;
+export default Tabs;
